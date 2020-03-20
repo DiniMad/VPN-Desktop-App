1f48af49dff89a6f9253248f1c6475eaf9d1ae9a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-20 23:07:24 +0330

    A converter to convert CheckBox IsChekcked property to text.

---
+++ GlobVpn/Views/Value Converters/ConnectButtonIsCheckedToTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GlobVpn.ValueConverters
+{
+    class ConnectButtonIsCheckedToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is null) return "Stop";
+            var isChecked=(bool) value;
+            return isChecked ? "Connecting..." : "Start";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
