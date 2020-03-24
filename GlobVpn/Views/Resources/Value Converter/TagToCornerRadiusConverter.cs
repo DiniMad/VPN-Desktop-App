f7c5bf87affc2b584ef1c0deaf0cb0e1a6d7d96f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:26:44 +0430

    A value converter for converting tag to corner radius.

---
+++ GlobVpn/Views/Resources/Value Converter/TagToCornerRadiusConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GlobVpn.Views.Resources.Value_Converter
+{
+    class TagToCornerRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
