11648af1bbbba2bc2264a3e1562abbe72355ccdf
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:41:20 +0430

    Create the SubscribePlane page.

---
+++ GlobVpn/Views/SubscribePlans.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlobVpn.Views
+{
+    /// <summary>
+    /// Interaction logic for SubscribePlans.xaml
+    /// </summary>
+    public partial class SubscribePlans : Page
+    {
+        public Window Window => Window.GetWindow(this);
+
+        public SubscribePlans()
+        {
+            InitializeComponent();
+
+            // Different plans should bind to different methods
+            YearPlane.ButtonBuyClick = Button_Click;
+            YearPlane2.ButtonBuyClick = Button_Click;
+            YearPlane3.ButtonBuyClick = Button_Click;
+            YearPlane4.ButtonBuyClick = Button_Click;
+        }
+        public Action<object, RoutedEventArgs> ButtonBuyClick { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e, int planTitle, int dolarPrice, int rialPrice)
+        {
+            (Window as MainWindow).SetModalContent(new CheckOut(planTitle,dolarPrice,rialPrice));
+        }
+    }
+}
