8f2bcc48e72524ec5a455a0f578967fa509a0984
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:38:25 +0430

    Update Register xaml and its code behind.

---
+++ GlobVpn/Views/Register.xaml.cs
@@ -0,0 +1,43 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GlobVpn.Views
+{
+    public partial class Register : Page
+    {
+        public Window Window => Window.GetWindow(this);
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Window.DragMove();
+        }
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Window.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            Window.WindowState = Window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+        private void ButtonRegister_OnClick(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow) Window).WindowContent = MainWindow.Windows.PrimaryPanel;
+        }
+
+        private void ButtonGoToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow) Window).WindowContent = MainWindow.Windows.Login;
+        }
+    }
+}
