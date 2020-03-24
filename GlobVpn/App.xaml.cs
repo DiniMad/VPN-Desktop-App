e4cf3f680a07793d41144382a5eacc9a371d5f9a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:35:30 +0430

    Update App.xaml and App.xaml.cs to contains basic and theme resources.
    And also have ability to change theme.

--- GlobVpn/App.xaml.cs
+++ GlobVpn/App.xaml.cs
@@ -1,9 +1,42 @@
-﻿namespace GlobVpn
+﻿using System;
+using System.Windows;
+
+namespace GlobVpn
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SetUpTheme();
+            new MainWindow().ShowDialog();
+        }
+
+        private void SetUpTheme()
+        {
+            var isDarkTheme=GlobVpn.Properties.Settings.Default.DarkTheme;
+
+            var themeName=(!isDarkTheme)?"DefaultThemeResources.xaml":"DarkThemeResources.xaml";
+            var uri = new Uri($"Views/Resources/{themeName}", UriKind.Relative);
+
+            Current.Resources.MergedDictionaries[0].MergedDictionaries.Clear();
+            Current.Resources.MergedDictionaries[0].MergedDictionaries.Add(new ResourceDictionary{Source = uri});
+        }
+
+        public void ChangeTheme()
+        {
+            var isDarkTheme=GlobVpn.Properties.Settings.Default.DarkTheme;
+
+            var themeName=(isDarkTheme)?"DefaultThemeResources.xaml":"DarkThemeResources.xaml";
+            var uri = new Uri($"Views/Resources/{themeName}", UriKind.Relative);
+
+            Current.Resources.MergedDictionaries[0].MergedDictionaries.Clear();
+            Current.Resources.MergedDictionaries[0].MergedDictionaries.Add(new ResourceDictionary { Source = uri });
+
+            GlobVpn.Properties.Settings.Default.DarkTheme = !isDarkTheme;
+            GlobVpn.Properties.Settings.Default.Save();
+        }
     }
 }
