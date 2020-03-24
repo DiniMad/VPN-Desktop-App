fcafeee65ae573d30763038af854b457b5f5ab5c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:39:49 +0430

    Update the MainWindow to contains two frame for content and modal.
    And also ability to change the frames.

--- GlobVpn/MainWindow.xaml.cs
+++ GlobVpn/MainWindow.xaml.cs
@@ -3,6 +3,14 @@ using System.Windows;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
 using System;
+using GlobVpn.Views;
+using System.Windows.Navigation;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace GlobVpn
 {
@@ -11,21 +19,121 @@ namespace GlobVpn
     /// </summary>
     public partial class MainWindow
     {
+        public enum Windows
+        {
+            Login,
+            Register,
+            PrimaryPanel,
+            SubscribePlans,
+        }
+
+        public Windows WindowContent
+        {
+            set
+            {
+                Page newPage;
+                switch (value)
+                {
+                    case Windows.Login:
+                        newPage = new Login();
+                        break;
+                    case Windows.Register:
+                        newPage = new Register();
+                        break;
+                    case Windows.PrimaryPanel:
+                        newPage = new PrimaryPanel();
+                        break;
+                    case Windows.SubscribePlans:
+                        newPage = new SubscribePlans();
+                        break;
+                    default:
+                        throw new AggregateException();
+                }
+                //FrameContent.Content = newPage;
+                NavigateToWindow(newPage);
+            }
+        }
+
+        private bool IsWindowOpacityAnimationRunning;
+        private bool IsWindowMarginAnimationRunning;
+        private TimeSpan AnimationsDuration=TimeSpan.FromSeconds(.8);
+        private int MarginAnimationOffset=600;
+     
+
         public MainWindow()
         {
             InitializeComponent();
+            FrameContent.Content = new Login();
         }
-        private void Window_SourceInitialized(object sender, System.EventArgs e)
+
+        private void NavigateToWindow(Page newPage)
+        {
+            if (IsWindowOpacityAnimationRunning || IsWindowMarginAnimationRunning)
+                return;
+            //IsWindowMarginAnimationRunning = true;
+            IsWindowOpacityAnimationRunning = true;
+            var marginAnimation = new ThicknessAnimation
+            {
+                To = new Thickness(-MarginAnimationOffset, 0, MarginAnimationOffset, 0),
+                Duration = AnimationsDuration
+            };
+            var opacityAnimation = new DoubleAnimation
+            {
+                To = 0,
+                Duration = AnimationsDuration
+            };
+            marginAnimation.Completed += (sender, e) => MarginAnimation_Completed(sender, e, newPage);
+            opacityAnimation.Completed += (sender, e) => OpacityAnimation_Completed(sender, e, newPage);
+            //FrameContent.BeginAnimation(MarginProperty, marginAnimation);
+            FrameContent.BeginAnimation(OpacityProperty, opacityAnimation);
+        }
+        private void MarginAnimation_Completed(object sender, EventArgs e, Page newPage)
+        {
+            IsWindowMarginAnimationRunning = false;
+            RunSecondPartOfAnimation(newPage);
+
+
+        }
+        private void OpacityAnimation_Completed(object sender, EventArgs e, Page newPage)
+        {
+            IsWindowOpacityAnimationRunning = false;
+            RunSecondPartOfAnimation(newPage);
+        }
+        private void RunSecondPartOfAnimation(Page newPage)
+        {
+            if (IsWindowMarginAnimationRunning || IsWindowOpacityAnimationRunning)
+                return;
+
+            FrameContent.Content = newPage;
+            //Task.Delay(300);
+            var marginAnimation = new ThicknessAnimation
+            {
+                From= new Thickness(MarginAnimationOffset, 0, -MarginAnimationOffset, 0),
+                To = new Thickness(0),
+                Duration = AnimationsDuration
+            };
+            var opacity = new DoubleAnimation
+            {
+                To = 1,
+                Duration = AnimationsDuration
+            };
+            //FrameContent.BeginAnimation(MarginProperty, marginAnimation);
+            FrameContent.BeginAnimation(OpacityProperty, opacity);
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
         {
             //WindowAspectRatio.Register((Window)sender);
         }
 
-        
+        public void SetModalContent(Page content) => FrameModal.Content = content;
     }
+
     internal class WindowAspectRatio
     {
         private double _ratio;
-        Window _window;
+        Window         _window;
+
         private WindowAspectRatio(Window window)
         {
             _window = window;
@@ -54,11 +162,11 @@ namespace GlobVpn
         {
             public IntPtr hwnd;
             public IntPtr hwndInsertAfter;
-            public int x;
-            public int y;
-            public int cx;
-            public int cy;
-            public int flags;
+            public int    x;
+            public int    y;
+            public int    cx;
+            public int    cy;
+            public int    flags;
         }
 
         private IntPtr DragHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handeled)
@@ -67,7 +175,7 @@ namespace GlobVpn
                 return IntPtr.Zero;
             if ((WM)msg == WM.WINDOWPOSCHANGING)
             {
-                WINDOWPOS position = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+                WINDOWPOS position = (WINDOWPOS) Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
 
                 if ((position.flags & (int)SWP.NoMove) != 0 ||
                     HwndSource.FromHwnd(hwnd).RootVisual == null)
