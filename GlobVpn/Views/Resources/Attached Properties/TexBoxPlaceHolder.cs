b3ddb629bea08b1b64c766cd94bec13ca68e594f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:25:56 +0430

    Add an attached property for input elements place holder.

---
+++ GlobVpn/Views/Resources/Attached Properties/TexBoxPlaceHolder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GlobVpn.Views.Resources.Attached_Properties
+{
+    class TexBoxPlaceHolder : DependencyObject
+    {
+
+        public static readonly DependencyProperty PlaceHolderProperty = DependencyProperty
+            .RegisterAttached("PlaceHolder", typeof(string), typeof(TexBoxPlaceHolder));
+
+
+
+        public static void SetPlaceHolder(UIElement element, string value)
+
+        {
+
+            element.SetValue(PlaceHolderProperty, value);
+
+        }
+
+
+
+        public static string GetPlaceHolder(UIElement element)
+
+        {
+
+            return (string)element.GetValue(PlaceHolderProperty);
+
+        }
+
+    }
+}
