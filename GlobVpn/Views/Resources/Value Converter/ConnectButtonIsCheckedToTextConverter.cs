77a29716d62012776f147ec85447124ec583d196
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-24 10:29:59 +0430

    Change ConnectButtonIsCheckedToTextConverter namespace.

--- GlobVpn/Views/Resources/Value Converter/ConnectButtonIsCheckedToTextConverter.cs
+++ GlobVpn/Views/Resources/Value Converter/ConnectButtonIsCheckedToTextConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace GlobVpn.ValueConverters
+namespace GlobVpn.Views.Resources.Value_Converter
 {
     class ConnectButtonIsCheckedToTextConverter : IValueConverter
     {
