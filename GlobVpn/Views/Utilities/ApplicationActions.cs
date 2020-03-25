1d73ded1f285c4f1e1a60152cb0f38439a17ebd8
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 08:54:20 +0430

    Add setting modal content functionality to the ApplicationActions.

--- GlobVpn/Views/Utilities/ApplicationActions.cs
+++ GlobVpn/Views/Utilities/ApplicationActions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GlobVpn.Views.Utilities
 {
@@ -16,14 +17,19 @@ namespace GlobVpn.Views.Utilities
     }
     static class ApplicationActions
     {
-      
+        public static MainWindow MainWindow => (Application.Current.MainWindow as MainWindow);
+
         public static void SetContent(WindowContent content)
         {
-            (Application.Current.MainWindow as MainWindow).SetContent = content;
+            MainWindow.SetContent = content;
         }
         public static void ChangeTheme()
         {
             ((App)Application.Current).ChangeTheme();
+        }
+        public static void SetModalContent(Page content)
+        {
+            MainWindow.SetModalContent = content;
         }
     }
 }
