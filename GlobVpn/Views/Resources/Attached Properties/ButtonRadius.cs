8fe9302c343a4267f14783bd5aca60cf03cd039f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 19:14:05 +0430

    Create a attached property for button corner radius.

---
+++ GlobVpn/Views/Resources/Attached Properties/ButtonRadius.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GlobVpn.Views.Resources.Attached_Properties
+{
+    class ButtonRadius : DependencyObject
+    {
+        public static readonly DependencyProperty RadiusProperty = DependencyProperty
+            .RegisterAttached("Radius", typeof(CornerRadius), typeof(ButtonRadius));
+
+        public static void SetRadius(UIElement element, CornerRadius value) =>
+            element.SetValue(RadiusProperty, value);
+        public static CornerRadius GetRadius(UIElement element) => 
+            (CornerRadius) element.GetValue(RadiusProperty);
+    }
+}
