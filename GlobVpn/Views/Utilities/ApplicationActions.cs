8ca8ac4883912bbadb9d1d25822cb83cb4eaec34
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 07:56:29 +0430

    Create a general class for changing content and theme.

---
+++ GlobVpn/Views/Utilities/ApplicationActions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GlobVpn.Views.Utilities
+{
+    public enum WindowContent
+    {
+        Login,
+        Register,
+        PrimaryPanel,
+        SubscribePlans,
+    }
+    static class ApplicationActions
+    {
+      
+        public static void SetContent(WindowContent content)
+        {
+            (Application.Current.MainWindow as MainWindow).SetContent = content;
+        }
+        public static void ChangeTheme()
+        {
+            ((App)Application.Current).ChangeTheme();
+        }
+    }
+}
