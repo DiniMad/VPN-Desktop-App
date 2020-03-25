44094b422d96a18022c81570d8b0b270a7c2bb66
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 18:57:54 +0430

    Create an attached property for input icon.

---
+++ GlobVpn/Views/Resources/Attached Properties/InputIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GlobVpn.Views.Resources.Attached_Properties
+{
+    class InputIcon : DependencyObject
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty
+            .RegisterAttached("Icon", typeof(BitmapImage), typeof(InputIcon));
+
+        public static void SetIcon(UIElement element, BitmapImage value) =>
+            element.SetValue(IconProperty, value);
+        public static BitmapImage GetIcon(UIElement element) => 
+            (BitmapImage) element.GetValue(IconProperty);
+    }
+}
