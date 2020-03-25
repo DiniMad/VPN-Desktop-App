246141ccd1f8aebd3e41fd61ee36f23edcf7351a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 08:18:33 +0430

    Remove redundant code from the MainWindow.

--- GlobVpn/MainWindow.xaml.cs
+++ GlobVpn/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
@@ -64,74 +64,6 @@ namespace GlobVpn
             FrameContent.Content = new Login();
         }
 
-
-        private void Window_SourceInitialized(object sender, EventArgs e)
-        {
-            //WindowAspectRatio.Register((Window)sender);
-        }
-
         public void SetModalContent(Page content) => FrameModal.Content = content;
-    }
-
-    internal class WindowAspectRatio
-    {
-        private double _ratio;
-        Window         _window;
-
-        private WindowAspectRatio(Window window)
-        {
-            _window = window;
-            _ratio = window.Width / window.Height;
-            ((HwndSource)HwndSource.FromVisual(window)).AddHook(DragHook);
-        }
-
-        public static void Register(Window window)
-        {
-            new WindowAspectRatio(window);
-        }
-
-        internal enum WM
-        {
-            WINDOWPOSCHANGING = 0x0046,
-        }
-
-        [Flags()]
-        public enum SWP
-        {
-            NoMove = 0x2,
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct WINDOWPOS
-        {
-            public IntPtr hwnd;
-            public IntPtr hwndInsertAfter;
-            public int    x;
-            public int    y;
-            public int    cx;
-            public int    cy;
-            public int    flags;
-        }
-
-        private IntPtr DragHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handeled)
-        {
-            if (_window.WindowState != WindowState.Normal)
-                return IntPtr.Zero;
-            if ((WM)msg == WM.WINDOWPOSCHANGING)
-            {
-                WINDOWPOS position = (WINDOWPOS) Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
-
-                if ((position.flags & (int)SWP.NoMove) != 0 ||
-                    HwndSource.FromHwnd(hwnd).RootVisual == null)
-                    return IntPtr.Zero;
-
-                position.cx = (int)(position.cy * _ratio);
-
-                Marshal.StructureToPtr(position, lParam, true);
-                handeled = true;
-            }
-
-            return IntPtr.Zero;
-        }
     }
 }
