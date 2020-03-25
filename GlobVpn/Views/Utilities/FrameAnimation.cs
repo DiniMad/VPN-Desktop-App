77b59ddf74cb69d9b56dbdb79bc5974b5481f26a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-25 08:14:26 +0430

    Create a class to encapsulate the changing frame content with animation.

---
+++ GlobVpn/Views/Utilities/FrameAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace GlobVpn.Views.Utilities
+{
+    class FrameAnimation
+    {
+        private TimeSpan _animationsDuration { get; }
+        private Frame _frame { get; }
+
+        public FrameAnimation(Frame frame, double animationDuration)
+        {
+            _frame = frame;
+            _animationsDuration = TimeSpan.FromSeconds(animationDuration);
+        }
+        public void ChangeFrameContentWithAnimation(Page newPage)
+        {
+            var opacityAnimation = new DoubleAnimation
+            {
+                To = 0,
+                Duration = _animationsDuration
+            };
+            opacityAnimation.Completed += (sender, e) => OpacityAnimation_Completed(newPage);
+            _frame.BeginAnimation(UIElement.OpacityProperty, opacityAnimation);
+        }
+
+        private void OpacityAnimation_Completed(Page newPage)
+        {
+            _frame.Content = newPage;
+            var opacity = new DoubleAnimation
+            {
+                To = 1,
+                Duration = _animationsDuration
+            };
+            _frame.BeginAnimation(UIElement.OpacityProperty, opacity);
+        }
+    }
+}
